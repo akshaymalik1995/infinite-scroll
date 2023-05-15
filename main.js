5809b6f3d7cc83c4faac008d617c2754d3acca87
akshaymalik1995 <akshaymalik191@gmail.com>
2023-05-15 10:27:02 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,68 @@
+// Get the body element
+const body = document.querySelector('body');
+// Create a list of 50 tailwind colors
+const colors = new Set([
+
+    "bg-red-400",
+    "bg-red-500",
+    "bg-red-600",
+    "bg-red-700",
+    
+    
+    "bg-blue-400",
+    "bg-blue-500",
+    "bg-blue-600",
+    "bg-blue-700",
+    
+    "bg-green-400",
+    "bg-green-500",
+    "bg-green-600",
+    "bg-green-700",
+
+  
+    "bg-yellow-400",
+    "bg-yellow-500",
+    "bg-yellow-600",
+    "bg-yellow-700",
+
+  
+    "bg-pink-400",
+    "bg-pink-500",
+    "bg-pink-600",
+    "bg-pink-700",
+
+
+    "bg-purple-400",
+    "bg-purple-500",
+    "bg-purple-600",
+    "bg-purple-700",
+]);
+
+let timeoutId;
+function handleScroll() {
+    if (timeoutId) {
+        clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+        appendElement();
+    }, 10);
+}
+
+// Create a function appendElement
+function appendElement() {
+    // Create a new element div
+    const div = document.createElement('div');
+    div.innerHTML = `<div class="item w-screen h-screen"></div>`;
+    // Select a random color from the colors array
+    const randomColor = [...colors][Math.floor(Math.random() * colors.size)];
+    // Add the random color to the div
+    div.classList.add(randomColor);
+    // Append the div to the body
+    body.appendChild(div);
+}
+
+// Add a scroll event listener to the window
+window.addEventListener('scroll', () => {
+    handleScroll();
+});
+
